72e19bb90fa89eec3e43a928822949166b41f7f9
Dovile-D <dovile.d.git@gmail.com>
2022-09-20 18:24:45 +0300

    add spring-boot-starter-mail dependency

--- src/main/java/com/demo/udema/service/EmailSenderService.java
+++ src/main/java/com/demo/udema/service/EmailSenderService.java
@@ -1,4 +1,10 @@
 package com.demo.udema.service;
 
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service("emailSenderService")
 public class EmailSenderService {
+
+    private JavaMailSender javaMailSender;
 }
