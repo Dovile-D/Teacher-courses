757329c2256c2440673342807af2e199d076117d
Dovile-D <dovile.d.git@gmail.com>
2022-09-20 18:11:22 +0300

    modify VerificationToken entity

--- src/main/java/com/demo/udema/security/VerificationToken.java
+++ src/main/java/com/demo/udema/security/VerificationToken.java
@@ -3,19 +3,59 @@ package com.demo.udema.security;
 import com.demo.udema.entity.User;
 
 import javax.persistence.*;
+import java.util.Date;
+import java.util.UUID;
 
 @Entity
 public class VerificationToken {
 
     @Id
-    @GeneratedValue(strategy =  GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="token_id")
+    private long tokenid;
 
-    private  String token;
+    @Column(name="verification_token")
+    private String verificationToken;
 
-    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER )
-    @JoinColumn(name = "user_id", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
     private User user;
+
+    public VerificationToken(User user) {
+        this.user = user;
+        createdDate = new Date();
+        verificationToken = UUID.randomUUID().toString();
+    }
+
+    public VerificationToken() {
+    }
+
+    public long getTokenid() {
+        return tokenid;
+    }
+
+    public void setTokenid(long tokenid) {
+        this.tokenid = tokenid;
+    }
+
+    public String getVerificationToken() {
+        return verificationToken;
+    }
+
+    public void setVerificationToken(String verificationToken) {
+        this.verificationToken = verificationToken;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
 
     public User getUser() {
         return user;
@@ -24,6 +64,4 @@ public class VerificationToken {
     public void setUser(User user) {
         this.user = user;
     }
-
-
 }
