ec505e977268deedd40fece3ef1a670a2e9113e0
Dovile-D <dovile.d.git@gmail.com>
2022-09-20 19:25:25 +0300

    add @Repository annotation to UserRepository

--- src/main/java/com/demo/udema/repositoryDAO/UserRepository.java
+++ src/main/java/com/demo/udema/repositoryDAO/UserRepository.java
@@ -4,9 +4,11 @@ import com.demo.udema.entity.CourseReviews;
 import com.demo.udema.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
     User findById(int id);
